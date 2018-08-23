b37d13f43649d8579bbac1e5a2ad5eb4cd63ddb9
Nicholas Murray <murray.nicholas@gmail.com>
2018-08-23 18:46:31 +0100

    Changed to inline templates and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <h1>Store Inline Template</h1>
+  `,
+  styles: [`
+    h1 { color: hotpink; }
+  `]
 })
 export class AppComponent {
   title = 'store';
