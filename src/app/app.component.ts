f4378b2ea456b7fe75352d273cec9069aa9556f4
Nicholas Murray <murray.nicholas@gmail.com>
2018-08-28 22:43:50 +0200

    Created Application Layout With Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>Store Inline Template</h1>
-  `,
-  styles: [`
-    h1 { color: hotpink; }
-  `]
+    <router-outlet></router-outlet>
+  `
 })
 export class AppComponent {
   title = 'store';
