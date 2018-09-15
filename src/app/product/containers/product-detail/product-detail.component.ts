0a969f186ae54baa7069d9a9c7c2d77cc48eb371
Nicholas Murray <murray.nicholas@gmail.com>
2018-09-15 21:51:58 +0100

    Now outputting metadata tags

--- src/app/product/containers/product-detail/product-detail.component.ts
+++ src/app/product/containers/product-detail/product-detail.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../../models/product';
 import { ProductService } from '../../services/product.service';
-import { map } from 'rxjs/operators';
-
+import { map, tap } from 'rxjs/operators';
+import { UiService } from '../../../ui/services/ui.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -11,17 +11,27 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./product-detail.component.scss']
 })
 export class ProductDetailComponent implements OnInit {
-
   public product: Product = new Product();
 
-  constructor(private route: ActivatedRoute,
+  constructor(
+    private route: ActivatedRoute,
+    private ui: UiService,
   ) { }
 
   ngOnInit() {
     this.route.data
       .pipe(
-        map(data => data['product'])
+        map(data => data['product']),
+        tap(product => this.metaData(product)),
       )
       .subscribe(res => this.product = res);
+  }
+
+  metaData(product: Product) {
+    this.ui.setMetaData({
+      title: `${product.name} for only $${product.price}`,
+      description: product.description,
+      image: product.image,
+    });
   }
 }
--- src/app/product/containers/product-list/product-list.component.ts
+++ src/app/product/containers/product-list/product-list.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../../models/product';
 import { ActivatedRoute } from '@angular/router';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
+import { UiService } from '../../../ui/services/ui.service';
 
 @Component({
   selector: 'app-product-list',
@@ -9,15 +10,24 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
-
   public products: Product[] = [];
 
-  constructor(private route: ActivatedRoute) { }
-
-  ngOnInit() {
-    this.route.data.pipe(
-      map(data => data['products'])
-    ).subscribe(res => this.products = res);
+  constructor(private route: ActivatedRoute, private ui: UiService) {
   }
 
+  ngOnInit() {
+    this.route.data
+      .pipe(
+        map(data => data['products']),
+        tap(products => this.metaData(products)),
+      )
+      .subscribe(res => this.products = res);
+  }
+
+  metaData(products: Product[]) {
+    this.ui.setMetaData({
+      title: 'Products',
+      description: `Check out our collection of ${products.length} products`
+    });
+  }
 }
